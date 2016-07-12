5fa0304a55fe9e2b2d4eb7129ed522bd4ea7f23a
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2016-07-12 13:33:16 -0400

    util::ancestral_walk & constains_substring

--- matchwell/util.py
+++ matchwell/util.py
@@ -1,6 +1,23 @@
 from collections import Iterable
 
 
+def contains_substring(substr, coll, inverse=False):
+    """Return True if substr is contained within any element
+    in the collection.
+
+    Args:
+        substr (str): Substring to search for.
+        coll (Iterable[str]): Iterable of strings
+        inverse (bool): Invert matching; return hits when substr
+            is _not_ present.
+    Returns:
+        True if substring was found, else False.
+    """
+    if inverse:
+        return any(True for x in coll if substr not in x)
+    return any(True for x in coll if substr in x)
+
+
 def walk(val, level=0, leaves=True):
     """Walk a structure made of a dicts/lists."""
     if isinstance(val, dict):
@@ -9,10 +26,20 @@ def walk(val, level=0, leaves=True):
             yield from walk(v, level + 1, leaves=leaves)
     elif isinstance(val, list):
         for item in val:
-
             yield from walk(item, level + 1, leaves=leaves)
     elif leaves:
         yield val, level
+
+
+def ancestral_walk(struct):
+    """Walk a structure yielding (parent, current) tuples."""
+    stack = []
+    stack.append('')  # Root.
+    for l, i in walk(struct):
+        while len(stack) > i + 1:
+            stack.pop()  # Reset our stack's context
+        stack.append(l)
+        yield (stack[i], stack[i+1])
 
 
 def tree(struct, filtr=''):
